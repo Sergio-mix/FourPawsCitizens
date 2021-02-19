ac2f8db08d1a9c4e55c56392a523c05446289c93
MICKISS <masierram@unbosque.edu.co>
2021-02-19 18:28:12 -0500

    Punto3 V1

--- src/edu/unbosque/fourpawscitizens/Main.java
+++ src/edu/unbosque/fourpawscitizens/Main.java
@@ -7,6 +7,21 @@ import javax.swing.*;
 public class Main {
     public static void main(String[] args) {
         Manager manager = new Manager();
-        manager.leer();
+        long microChip= 0000000000003456;
+        String species="FELINO";
+        String sex ="MACHO";
+        String size ="GRANDE";
+        boolean potentDangerous = true;
+        String neighborhood= "USAQUEN";
+        String newMicroChip = microChip + "";
+        String newsMicroChip = newMicroChip.substring(12, 15);
+        String newSpecies = species.substring(0, 1);
+        String newSex = sex.substring(0, 1);
+        String newSize = size.substring(0, 1);
+        String newPotentDangerous = potentDangerous + "";
+        String newsPotentDangerous = newPotentDangerous.substring(0, 1);
+
+        String id = newsMicroChip + "-" + newSpecies + "-" + newSex + "-" + newSize+ "-" + newsPotentDangerous + "-" + neighborhood;
+        System.out.println(id);
     }
 }
--- src/edu/unbosque/fourpawscitizens/model/Manager.java
+++ src/edu/unbosque/fourpawscitizens/model/Manager.java
@@ -9,31 +9,42 @@ public class Manager {
 
     public static final String SEPARADOR = ",";
 
-   public void leer(){
+    public void leer() {
 
-       BufferedReader bufferLectura = null;
-       try {
-           bufferLectura = new BufferedReader(new FileReader("archivo.csv"));
-           String linea = bufferLectura.readLine();
-           while (linea != null) {
-               String[] campos = linea.split(SEPARADOR);
-               System.out.println(Arrays.toString(campos));
-               linea = bufferLectura.readLine();
-           }
-       }
-       catch (IOException e) {
-           e.printStackTrace();
-       }
-       finally {
-           if (bufferLectura != null) {
-               try {
-                   bufferLectura.close();
-               }
-               catch (IOException e) {
-                   e.printStackTrace();
-               }
-           }
-       }
-   }
+        BufferedReader bufferLectura = null;
+        try {
+            bufferLectura = new BufferedReader(new FileReader("archivo.csv"));
+            String linea = bufferLectura.readLine();
+            while (linea != null) {
+                String[] campos = linea.split(SEPARADOR);
+                System.out.println(Arrays.toString(campos));
+                linea = bufferLectura.readLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (bufferLectura != null) {
+                try {
+                    bufferLectura.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public String assignID(long microChip, String species, String sex, String size, boolean potentDangerous, String neighborhood) {
+        String newMicroChip = microChip + "";
+        String newsMicroChip = newMicroChip.substring(12, 15);
+        String newSpecies = species.substring(0, 1);
+        String newSex = sex.substring(0, 1);
+        String newSize = size.substring(0, 1);
+        String newPotentDangerous = potentDangerous + "";
+        String newsPotentDangerous = newPotentDangerous.substring(0, 1);
+
+        String id = newsMicroChip + "-" + newSpecies + "-" + newSex + "-" + newSize+ "-" + newsPotentDangerous + "-" + neighborhood;
+        return id.toUpperCase();
+    }
+
 }
 
