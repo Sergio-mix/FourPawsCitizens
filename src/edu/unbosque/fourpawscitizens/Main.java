b4cada4aa0edbf4951e1b31052d6328add5067a6
MICKISS <masierram@unbosque.edu.co>
2021-02-19 16:37:04 -0500

    Prueba 3

--- src/edu/unbosque/fourpawscitizens/Main.java
+++ src/edu/unbosque/fourpawscitizens/Main.java
@@ -5,5 +5,6 @@ import javax.swing.*;
 public class Main {
     public static void main(String[] args) {
         JOptionPane.showConfirmDialog(null,"Miguel gay?");
+        System.out.println(".l.");
     }
 }
